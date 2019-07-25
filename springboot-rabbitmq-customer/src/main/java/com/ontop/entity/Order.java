5bfe87b667b73c1b20302f6276a813f1e58e5718
smart <smart@onTop.com>
2019-07-25 09:51:21 +0800

    add rabbitmq-customer function

---
+++ springboot-rabbitmq-customer/src/main/java/com/ontop/entity/Order.java
@@ -0,0 +1,16 @@
+package com.ontop.entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order implements Serializable{
+	private static final long serialVersionUID = 7293986973312565395L;
+	private String id;
+	private String name;
+}
